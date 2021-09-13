2cf155c2d09cfaf21e04f6225a7fa8bddf8d7195
Jaeyong Nam <skawodyd789@korea.ac.kr>
2021-09-13 15:18:54 +0900

    Miniter Api

---
+++ config.py
@@ -0,0 +1,27 @@
+db = {
+    'user': 'root',
+    'password': 'akqlshrl789',
+    'host': 'localhost',
+    'port': 13306,
+    'database': 'miniter'
+}
+DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
+JWT_SECRET_KEY = 'SOME_SUPER_SECRET_KEY'
+
+config = {
+    'DB_URL': f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8",
+    'JWT_SECRET_KEY': 'SOME_SUPER_SECRET_KEY'
+}
+
+test_db = {
+    'user': 'test',
+    'password': 'test123',
+    'host': 'localhost',
+    'port': 13306,
+    'database': 'test_db'
+}
+
+test_config = {
+    'DB_URL': f"mysql+mysqlconnector://{test_db['user']}:{test_db['password']}@{test_db['host']}:{test_db['port']}/{test_db['database']}?charset=utf8",
+    'JWT_SECRET_KEY': 'SOME_SUPER_SECRET_KEY'
+}
